63f32a894b12087f57148df2ff0c56517594b84f
haya2001a <hayaalami0@gmail.com>
2022-10-01 21:22:15 +0300

    adding task 8

---
+++ app.js
@@ -0,0 +1,27 @@
+let count_alert=document.getElementById("count-alert");
+function openAlert(){
+    count_alert.classList.add("open-alert");
+    
+            var str = document.getElementById("input_text").value;
+            var charCounter=0;
+            var spaceCounter=0;
+            
+            for(var i=0 ;i<str.length;i++){
+                if(str[i] != " "){
+                    charCounter+=1;
+                }
+                if(str[i] == " "){
+                    spaceCounter+=1;
+                   
+                } 
+            }
+        document.getElementById("counter").innerHTML = "The text you entered consists of :  " ;
+        document.getElementById("charCounter").innerHTML = charCounter  +"    characters";
+        document.getElementById("spaceCounter").innerHTML = spaceCounter+"    spaces";
+        }
+    function closeAlert(){
+      count_alert.classList.remove("open-alert");
+    }
+function clear_text(){
+document.getElementById("input_text").value = "";
+}
